b6ddd0e29f2f75eb7262dc61e0e61f07b9c3d0de
Abdul Rasheed Jan <158552934+abdulrasheedjan@users.noreply.github.com>
2024-05-11 16:42:04 +0500

    Add files via upload

---
+++ muhammad islam/index.js
@@ -0,0 +1,20 @@
+// function calculateSavings() {
+//     var income = parseFloat(document.getElementById('income').value);
+//     var expenses = parseFloat(document.getElementById('expenses').value);
+
+//     if (isNaN(income) || isNaN(expenses)) {
+//         alert("Please enter valid numbers for income and expenses.");
+//         return;
+//     }
+
+//     var savings = income - expenses;
+//     var savingsElement = document.getElementById('savings');
+//     savingsElement.innerHTML = "<h3>Your Savings: PKR " + savings.toFixed(2) + "</h3>";
+// }
+// var a = Math.round(2.5)
+// var a = "2"
+// var b =  Number(a)
+// console.log(b)
+var integerString = 24;
+// var num = Number(integerString);
+console.log(typeof  integerString)
